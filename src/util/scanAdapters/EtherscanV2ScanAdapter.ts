4ae089f452db0f880f4fa607adbe6ccb0d5a15f0
Sam Holmes <sam@edge.app>
2025-10-23 16:10:19 -0700

    Abstract fetch to Etherscan API

--- src/util/scanAdapters/EtherscanV2ScanAdapter.ts
+++ src/util/scanAdapters/EtherscanV2ScanAdapter.ts
@@ -46,14 +46,17 @@ export function makeEtherscanV2ScanAdapter(
     if (apiKey != null) {
       params.set('apikey', apiKey)
     }
-    const response = await fetch(`${url}/v2/api?${params.toString()}`)
-    if (response.status !== 200) {
-      logger.error('scanAddress error', response.status, response.statusText)
+    const response = await fetchEtherscanV2(url, params)
+    if ('error' in response) {
+      logger.error(
+        'scanAddress error',
+        response.httpStatus,
+        response.httpStatusText
+      )
       return true
     }
-    const dataRaw = await response.json()
-    const data = asResult(dataRaw)
-    if (data.status === '1' && data.result.length > 0) {
+    const transactionData = asResult(response.json)
+    if (transactionData.status === '1' && transactionData.result.length > 0) {
       return true
     }
 
@@ -70,17 +73,16 @@ export function makeEtherscanV2ScanAdapter(
     if (apiKey != null) {
       tokenParams.set('apikey', apiKey)
     }
-    const tokenResponse = await fetch(`${url}/v2/api?${tokenParams.toString()}`)
-    if (tokenResponse.status !== 200) {
+    const tokenResponse = await fetchEtherscanV2(url, tokenParams)
+    if ('error' in tokenResponse) {
       logger.error(
         'scanAddress tokenTx error',
-        tokenResponse.status,
-        tokenResponse.statusText
+        tokenResponse.httpStatus,
+        tokenResponse.httpStatusText
       )
       return false
     }
-    const tokenDataRaw = await tokenResponse.json()
-    const tokenData = asResult(tokenDataRaw)
+    const tokenData = asResult(tokenResponse.json)
     if (tokenData.status === '1' && tokenData.result.length > 0) {
       return true
     }
@@ -93,3 +95,30 @@ const asResult = asObject({
   status: asString,
   result: asArray(asUnknown)
 })
+
+type EtherscanResult =
+  | {
+      json: unknown
+      httpStatus: number
+    }
+  | { error: boolean; httpStatus: number; httpStatusText: string }
+
+async function fetchEtherscanV2(
+  url: string,
+  params: URLSearchParams
+): Promise<EtherscanResult> {
+  const response = await fetch(`${url}/v2/api?${params.toString()}`)
+  if (response.status !== 200) {
+    return {
+      error: true,
+      httpStatus: response.status,
+      httpStatusText: response.statusText
+    }
+  }
+
+  const json = await response.json()
+  return {
+    json,
+    httpStatus: response.status
+  }
+}
